fe330fc233722a625a7b946eab26dd18fb83afbf
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-09-15 13:35:20 +0200

    Page 404

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import Articles from "./Articles";
 import Planete from "./Planete";
 import Layout from "./Layout";
+import NotFound from "./NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="*" element={<NotFound />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="/planete/:planeteId" element={<Planete />} />
         </Route>
