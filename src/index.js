2d915eb2fd1483692554d8cf36db6267ffc13c30
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-09-15 13:12:43 +0200

    Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Articles from './Articles';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Articles from "./Articles";
+import Planete from "./Planete";
+import Layout from "./Layout";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Articles />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/planete/:planeteId" element={<Planete />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
