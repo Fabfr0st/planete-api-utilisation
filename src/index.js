04cf374019d2a1aadb07cfa3380f6ef5a4f486fa
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-09-15 11:34:55 +0200

    Page articles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Articles from './Articles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Articles />
   </React.StrictMode>
 );
